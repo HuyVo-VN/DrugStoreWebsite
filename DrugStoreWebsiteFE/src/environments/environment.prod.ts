2b6031a349e1a3f0de7303972bf9f9461abe4fb3
huy vo <votanhuyy1504@gmail.com>
2026-04-13 23:42:35 +0700

    Update environment file for FE Service

--- DrugStoreWebsiteFE/src/environments/environment.prod.ts
+++ DrugStoreWebsiteFE/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authenApiUrl: 'http://drugstore-huyvo.duckdns.org',
-  dataApiUrl: 'http://drugstore-huyvo.duckdns.org',
-  aiApiUrl: 'http://drugstore-huyvo.duckdns.org',
+  authenApiUrl: 'https://drugstore-huyvo.duckdns.org',
+  dataApiUrl: 'https://drugstore-huyvo.duckdns.org',
+  aiApiUrl: 'https://drugstore-huyvo.duckdns.org',
   googleClientId: '412203333687-d7cu6on8ss5f1gic1qadjjvlo9t6jp3k.apps.googleusercontent.com'
 };
