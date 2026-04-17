81f93bfda44a292724561bae8d4f8107d8ecb97b
huy vo <votanhuyy1504@gmail.com>
2026-04-17 15:34:46 +0700

    Update Migarion file

---
+++ DrugStoreWebsiteAuthen/DrugStoreWebsiteAuthen.Infrastructure/Migrations/20260416173228_Add2FA.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrugStoreWebsiteAuthen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add2FA : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorSecret",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorSecret",
+                table: "AspNetUsers");
+        }
+    }
+}
